be6340f6f5a661fcde8bb584273687e824b601d1
Mashariq Khan <mashariq@outlook.com>
2019-07-06 00:49:43 -0500

    Added extra variable x to have type clarity and iterator

--- src/third.rs
+++ src/third.rs
@@ -1,5 +1,4 @@
 use std::rc::Rc;
-use core::borrow::BorrowMut;
 
 type Link<T> = Option<Rc<Node<T>>>;
 
@@ -24,8 +23,24 @@ impl<T> Node<T> {
     }
 }
 
-impl<T> List<T> {
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>
+}
 
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            let x = node.next.as_ref().map(|node| &**node);
+            self.next = x;
+            let x = &node.value;
+            x
+        })
+    }
+}
+
+impl<T> List<T> {
     fn new() -> Self {
         List {
             head: None,
@@ -33,11 +48,11 @@ impl<T> List<T> {
         }
     }
 
-    fn len(&self) -> u32{
+    fn len(&self) -> u32 {
         self.count
     }
 
-    fn append(&mut self, value: T) -> List<T> {
+    fn append(&self, value: T) -> List<T> {
         let new_count = self.count + 1;
         List {
             head: Some(Rc::new(Node {
@@ -48,21 +63,45 @@ impl<T> List<T> {
         }
     }
 
-    fn tail(&mut self) -> List<T> {
+    fn tail(&self) -> List<T> {
         let mut new_count = 0;
         if self.count > 0 {
             new_count = self.count - 1;
         }
         List {
             head: self.head.as_ref().and_then(|node| {
-                node.next.clone()
+                let x = node.next.clone();
+                x
             }),
             count: new_count,
         }
     }
 
     pub fn head(&self) -> Option<&T> {
-        self.head.as_ref().map(|node| &node.value)
+        self.head.as_ref().map(|node| {
+            let x = &node.value;
+            x
+        })
+    }
+
+    pub fn iter(&self) -> Iter<T> {
+        Iter {
+            next: self.head.as_ref().map(|node| {
+               let x =  &**node;
+                x
+            })
+        }
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut head = self.head.take();
+        while let Some(node) = head {
+            if let Ok(mut node) = Rc::try_unwrap(node) {
+                head = node.next.take();
+            } else { break; }
+        }
     }
 }
 
@@ -72,29 +111,39 @@ mod test {
 
     #[test]
     fn basics() {
-        let mut list = List::new();
+        let list = List::new();
         assert_eq!(list.head(), None);
-        assert_eq!(list.len(),0);
+        assert_eq!(list.len(), 0);
 
-        let mut list = list.append(1).append(2).append(3);
+        let  list = list.append(1).append(2).append(3);
         assert_eq!(list.head(), Some(&3));
-        assert_eq!(list.len(),3);
+        assert_eq!(list.len(), 3);
 
-        let mut list = list.tail();
+        let  list = list.tail();
         assert_eq!(list.head(), Some(&2));
-        assert_eq!(list.len(),2);
+        assert_eq!(list.len(), 2);
 
-        let mut list = list.tail();
+        let  list = list.tail();
         assert_eq!(list.head(), Some(&1));
-        assert_eq!(list.len(),1);
+        assert_eq!(list.len(), 1);
 
-        let mut list = list.tail();
+        let  list = list.tail();
         assert_eq!(list.head(), None);
-        assert_eq!(list.len(),0);
+        assert_eq!(list.len(), 0);
 
         // Make sure empty tail works
-        let mut list = list.tail();
+        let  list = list.tail();
         assert_eq!(list.head(), None);
-        assert_eq!(list.len(),0);
+        assert_eq!(list.len(), 0);
+    }
+
+    #[test]
+    fn iter() {
+        let list = List::new().append(1).append(2).append(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
     }
 }
